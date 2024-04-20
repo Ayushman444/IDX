b9df730749ec33fa20688183ff819597354cccf9
amangoyal018 <goyalamanmay18@gmail.com>
2024-04-20 15:34:58 +0530

    "editor page"

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,40 +1,37 @@
 // EditorPage.jsx
-import React, { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import toast from 'react-hot-toast';
 import { Box } from "@chakra-ui/react";
 import { CodeEditor } from "../components/ide/Editor";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import { CODE_SNIPPETS } from "../Constants";
 import { initSocket } from "../Socket";
-import { SideBar } from "../components/ide/SideBar";
-import { HStack } from "@chakra-ui/react";
+import { EditorContext, InputContext } from "../context/EditorContext";
 
 export const EditorPage = () => {
-  const reactNavigator = useNavigate();
   const socketRef = useRef(null);
   const codeRef = useRef("");
   const languageRef = useRef("");
   const [clients, setClients] = useState([]);
   const { roomId } = useParams();
-  const location = useLocation();
 
   useEffect(() => {
     const initializeSocket = async () => {
       const socket = await initSocket(); // Initialize socket connection
       socketRef.current = socket;
 
-      socket.emit("join", {
+      socket.emit('join', {
         roomId,
-        username: "dummyUser",
+        username: 'dummyUser',
       });
 
-      socket.on("joined", ({ clients, username, socketId }) => {
-        if (username !== "") {
+      socket.on('joined', ({ clients, username, socketId }) => {
+        if (username !== '') {
           toast.success(`${username} joined the room.`);
           console.log(`${username} joined`);
         }
         setClients(clients);
-        socket.emit("sync_code", {
+        socket.emit('sync_code', {
           code: codeRef.current,
           language: languageRef.current,
           socketId,
@@ -42,13 +39,19 @@ export const EditorPage = () => {
       });
 
       // Listening for disconnected
-      socket.on("disconnected", ({ socketId, username }) => {
-        toast.success(`${username} left the room.`);
-        setClients((prev) => {
-          return prev.filter((client) => client.socketId !== socketId);
-        });
-      });
+      socket.on(
+        'disconnected',
+        ({ socketId, username }) => {
+          toast.success(`${username} left the room.`);
+          setClients((prev) => {
+            return prev.filter(
+              (client) => client.socketId !== socketId
+            );
+          });
+        }
+      );
     };
+
 
     initializeSocket();
 
@@ -56,8 +59,8 @@ export const EditorPage = () => {
       // Cleanup function to disconnect socket when component unmounts
       if (socketRef.current) {
         socketRef.current.disconnect();
-        socketRef.current.off("joined");
-        socketRef.current.off("disconnected");
+        socketRef.current.off('joined');
+        socketRef.current.off('disconnected');
       }
     };
   }, [roomId]);
@@ -71,38 +74,15 @@ export const EditorPage = () => {
   // console.log(editorContext,inputContext);
   // console.log(codeRef);
 
-  async function copyRoomId() {
-    try {
-      await navigator.clipboard.writeText(roomId);
-      toast.success("Room ID has been copied to your clipboard");
-    } catch (err) {
-      toast.error("Could not copy the Room ID");
-      console.error(err);
-    }
-  }
-  function leaveRoom() {
-    reactNavigator("/");
-  }
-
   return (
-    <>
-      <div className="flex flex-row">
-        <Box>
-          <SideBar />
-        </Box>
-        <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-          <CodeEditor
-            roomId={roomId}
-            socket={socketRef}
-            onCodeChange={(code) => {
-              codeRef.current = code;
-            }}
-            onLanguageChange={(language) => {
-              languageRef.current = language;
-            }}
-          />
-        </Box>
-      </div>
-    </>
+    <EditorContext.Provider value={codeRef}>
+      <InputContext.Provider value={input}>
+
+      
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+      <CodeEditor roomId={roomId} socket={socketRef} onCodeChange={(code) => { codeRef.current = code; }} onLanguageChange={(language) => { languageRef.current = language; }} setInput = {setInput}/>
+    </Box>
+      </InputContext.Provider>
+    </EditorContext.Provider>
   );
 };
